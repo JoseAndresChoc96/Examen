92c9ee700862276f4887c3d37991687b3ca7d025
andres <joseandreschoc1996@gmail.com>
2018-11-19 11:52:58 -0600

    Examen final primer commit

---
+++ educacion/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+from django.contrib import admin
+
+# Create your models here.
+class Grado(models.Model):
+
+    nombre  =   models.CharField(max_length=30)
+    seccion = models.CharField(max_length=30)
+
+    def __str__(self):
+
+        return self.nombre
+
+class Materia(models.Model):
+    nombre    = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.nombre
+
+class Control (models.Model):
+    grado = models.ForeignKey(Grado, on_delete=models.CASCADE)
+    materia = models.ForeignKey(Materia, on_delete=models.CASCADE)
